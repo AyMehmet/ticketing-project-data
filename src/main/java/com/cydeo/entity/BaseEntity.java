bb62ac7b71cdeaba3fdd8d461eef5627ebc08248
aymehmet <aymehmet1976@gmail.com>
2022-11-26 13:55:32 +0300

    Part-2 Base Entity modified

--- src/main/java/com/cydeo/entity/BaseEntity.java
+++ src/main/java/com/cydeo/entity/BaseEntity.java
@@ -5,10 +5,7 @@ import lombok.Data;
 import lombok.Generated;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Data
@@ -23,9 +20,27 @@ public class BaseEntity {
 
     private Boolean isDeleted=false;
 
+    @Column(nullable = false,updatable = false)//ignore to update this ...
     private LocalDateTime insertDateTime;
+    @Column(nullable = false,updatable = false)
     private Long insertUserId;
+    @Column(nullable = false)
     private LocalDateTime lastUpdateDateTime;
+    @Column(nullable = false)
     private Long lastUpdateUserId;
+
+    @PrePersist
+    private void onPrePersist(){
+        this.insertDateTime=LocalDateTime.now();
+        this.lastUpdateDateTime=LocalDateTime.now();
+        this.insertUserId=1L;
+        this.lastUpdateUserId=1L;
+    }
+
+    @PreUpdate
+    private void onPreUpdate(){
+        this.lastUpdateDateTime=LocalDateTime.now();
+        this.lastUpdateUserId=1L;
+    }
 
 }
