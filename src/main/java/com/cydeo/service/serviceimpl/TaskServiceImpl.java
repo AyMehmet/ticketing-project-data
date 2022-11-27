73a719d4a1cb897421ee285cd887ff3f81e29108
aymehmet <aymehmet1976@gmail.com>
2022-11-28 01:37:50 +0300

    Part-3 task is created

---
+++ src/main/java/com/cydeo/service/serviceimpl/TaskServiceImpl.java
@@ -0,0 +1,42 @@
+package com.cydeo.service.serviceimpl;
+
+import com.cydeo.dto.TaskDTO;
+import com.cydeo.entity.Task;
+import com.cydeo.enums.Status;
+import com.cydeo.mapper.TaskMapper;
+import com.cydeo.repository.TaskRepository;
+import com.cydeo.service.TaskService;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class TaskServiceImpl implements TaskService {
+
+    private final TaskRepository taskRepository;
+    private final TaskMapper taskMapper;
+
+    public TaskServiceImpl(TaskRepository taskRepository, ModelMapper modelMapper, TaskMapper taskMapper) {
+        this.taskRepository = taskRepository;
+        this.taskMapper = taskMapper;
+
+    }
+
+    @Override
+    public List<TaskDTO> listAllTask() {
+        return taskRepository.findAll().stream().map(taskMapper::convertToDto).collect(Collectors.toList());
+    }
+
+    @Override
+    public void save(TaskDTO taskDTO) {
+        taskDTO.setTaskStatus(Status.OPEN);
+        taskDTO.setAssignedDate(LocalDate.now());
+        Task task = taskMapper.convertToEntity(taskDTO);
+        taskRepository.save(task);
+    }
+
+
+}
